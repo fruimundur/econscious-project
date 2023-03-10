b518f3a20000f05b51b539b3319be5f053e62df7
Fríðmundur Nattestad <fnattestad@gmail.com>
2023-03-10 08:52:52 +0000

    style slogan

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ export default function Start() {
   return (
     <div >
       <h1 className={styles.header}> Save the planet step-by-step </h1>
-      <p>Take small concrete steps to help combat climate change!</p>
+      <p className={styles.slogan}>Take small concrete steps to help combat climate change!</p>
       <Link className='start' href="/login">
         <button type="start">Start</button>
       </Link>
