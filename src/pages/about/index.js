df7c9d5c71e9b5c332296336a2bbcdba90dc8b3a
solveighelga <111748545+solveighelga@users.noreply.github.com>
2023-03-10 14:03:56 +0000

    created task and about page + made the pop up on task page

---
+++ src/pages/about/index.js
@@ -0,0 +1,16 @@
+import { useRouter } from 'next/router';
+import styles from '../../styles/About.module.scss'
+
+
+export default function Login() {
+const router = useRouter();
+
+  return (
+    <div>
+      <h1 className={styles.header}>How does it work?</h1>
+      <p className={styles.moreInfo}>
+        On your homepage you are presented with 10 random tasks. Each of these tasks include instructions on how to complete them, and they can all be completed in a short amount of time. Each task contributes to combating climate change. After you have completed a task you have the option of sharing your achievement on social media so you can encourage other people to do the same.
+      </p> 
+    </div>
+  );
+}
