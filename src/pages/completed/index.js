e0e045f37e789b07f8959d786c87f7015fec184a
Fríðmundur Nattestad <fnattestad@gmail.com>
2023-03-14 08:30:26 +0000

    fixed social media buttons

--- src/pages/completed/index.js
+++ src/pages/completed/index.js
@@ -2,7 +2,8 @@ import { useRouter } from 'next/router';
 import styles from '../../styles/Completed.module.scss'
 import Image from 'next/image'
 import placeholder from '../../../public/placeholder.jpg';
-import { FacebookMessengerShareButton, FacebookMessengerIcon } from 'next-share';
+import { FacebookShareButton, FacebookIcon, TwitterShareButton, TwitterIcon } from 'next-share';
+import nextConnect from 'next-connect';
 
 
 export default function Completed() {
@@ -20,14 +21,17 @@ const router = useRouter();
           <p className={styles.paragraph}>Information about how exactly this is impacting climate change.</p>
           <a className={styles.link} href="https://www.google.com/" target="_blank">Read more</a>
         </div>
+        <button className={styles.photoButton}>Add photo</button>
         <div className={styles.buttonContainer}>
-          <button className={styles.buttons}>Add photo</button>
-          <button className={styles.buttons}>Share</button>
+        <FacebookShareButton
+          url={'https://www.google.com/'}>
+          <FacebookIcon size={90} round />
+        </FacebookShareButton>
+        <TwitterShareButton
+          url={'https://www.google.com/'} >
+          <TwitterIcon size={90} round />
+        </TwitterShareButton>
         </div>
-        <FacebookMessengerShareButton
-          url={'http://localhost:3000/'} >
-          <FacebookMessengerIcon size={32} round />
-        </FacebookMessengerShareButton>
       </>
     );
   }
