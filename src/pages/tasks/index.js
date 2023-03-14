83fe7945a1d4eeb134f21837bca4cc86bdf287be
kvalls <kvalls.contact@gmail.com>
2023-03-14 11:25:09 +0000

    Updated tasks page, added fetching from Supabase

--- src/pages/tasks/index.js
+++ src/pages/tasks/index.js
@@ -3,8 +3,20 @@ import { useRouter } from 'next/router';
 import styles from '../../styles/tasks.module.scss'
 import React, { useState } from 'react';
 import Popup from '../modals';
+import { supabase } from '../../lib/supabaseClient';
 
-const MyComponent = () => {
+
+export async function getServerSideProps() {
+  let { data } = await supabase.from('tasks').select()
+
+  return {
+    props: {
+     tasks: data
+    },
+  }
+}
+
+const MyComponent = ({tasks}) => {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
  
   const handleOpenPopup = () => {
@@ -18,12 +30,15 @@ const MyComponent = () => {
   return (
     <div>
       <h1 className={styles.header}>Pick a task</h1>
-      <div>
-        <button className={styles.taskBtn} onClick={handleOpenPopup}>Name of Task</button>
+      {console.log(tasks)}
+      {tasks.map((task) => (
+      <div key={task.id}>
+        <button  className={styles.taskBtn} onClick={handleOpenPopup}>{task.taskname}</button>
         <Popup isOpen={isPopupOpen} onClose={handleClosePopup}>
           <p className={styles.text}>Text about your task is displayed here</p>
         </Popup>
       </div>
+       ))}
     </div>
   );
 };
