fc5a3047ba28dfb368738de2e1a7a29430654de6
solveighelga <111748545+solveighelga@users.noreply.github.com>
2023-03-16 12:59:14 +0000

    styled about page, needs responsiveness

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&family=Nunito:wght@200;900&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;700&family=Poppins:wght@200;500;900&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -15,19 +15,27 @@ const router = useRouter();
             className={styles.logo}
             src={logo}
             alt="Brand logo"
-          />
+          />     
       <Image
             className={styles.infoPhoto}
             src={infopage}
             alt="The Earth"
-          />
-      <h1 className={styles.header}>How does it work?</h1>
-      <p className={styles.moreInfo}>
-        On your homepage you are presented with 10 random tasks. Each of these tasks include instructions on how to complete them, and they can all be completed in a short amount of time. Each task contributes to combating climate change. After you have completed a task you have the option of sharing your achievement on social media so you can encourage other people to do the same.
-      </p> 
-      <Link className='start' href="/login">
-        <button className={styles.gotItBtn} type="start">Got it</button>
-      </Link>
+          />     
+        <h1 className={styles.header}>How does it work?</h1>
+        <p className={styles.moreInfo}>
+          On your homepage you are presented with 10 random tasks. Each of these tasks include instructions on how to complete them, and they can all be completed in a short amount of time.        
+        </p>
+        <p className={styles.moreInfo}>
+          Each task contributes to combating climate change.         
+        </p>
+        <p className={styles.moreInfo}>
+        After you have completed a task you have the option of sharing your achievement on social media so you can encourage other people to do the same.
+        </p>
+        <div className={styles.buttonStyle}>
+          <Link className='start' href="/login">
+            <button className={styles.gotItBtn} type="start">Got it</button>
+          </Link> 
+        </div>   
     </div>
   );
 }
