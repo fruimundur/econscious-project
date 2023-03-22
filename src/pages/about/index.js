162fd633f53d47dfc67caa34f066c2914c1532a8
solveighelga <111748545+solveighelga@users.noreply.github.com>
2023-03-22 15:26:49 +0000

    styled about page

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -11,16 +11,19 @@ const router = useRouter();
 
   return (
     <div className={styles.container}>
+      <div className={styles.headerContainer}>
       <Image
-            className={styles.logo}
-            src={logo}
-            alt="Brand logo"
-          />     
-      <Image
-            className={styles.infoPhoto}
-            src={infopage}
-            alt="The Earth"
-          />     
+              className={styles.logo}
+              src={logo}
+              alt="Brand logo"
+            />  
+      </div>           
+        <Image
+              className={styles.infoPhoto}
+              src={infopage}
+              alt="The Earth"
+            />  
+      <div className={styles.textContainer}>   
         <h1 className={styles.header}>How does it work?</h1>
         <p className={styles.moreInfo}>
           On your homepage you are presented with 10 random tasks. Each of these tasks include instructions on how to complete them, and they can all be completed in a short amount of time.        
@@ -31,6 +34,7 @@ const router = useRouter();
         <p className={styles.moreInfo}>
         After you have completed a task you have the option of sharing your achievement on social media so you can encourage other people to do the same.
         </p>
+      </div>
         <div className={styles.buttonStyle}>
           <Link className='start' href="/tasks">
             <button className={styles.gotItBtn} type="start">Got it</button>
